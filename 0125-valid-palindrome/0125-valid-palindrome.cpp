666b521bd9c1708b5e31d66778300ac77d3dd0e7
aamirhannan <56514008+aamirhannan@users.noreply.github.com>
2022-10-12 02:48:09 +0530

    Time: 7 ms (77.91%), Space: 7.4 MB (77.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,58 @@
-class Solution {
-public:
-    bool isPalindrome(string s) {
-        
-        string ans,output;
-        for(int i = 0; i < s.size(); i++)
+class Solution
+{
+    public:
+        bool isPalindrome(string s)
         {
-            if(s[i] >= 'A' and s[i] <= 'Z' or s[i] >= 'a' and s[i] <= 'z' or s[i] >= '0' and s[i] <= '9')
+
+           	//         string ans,output;
+           	//         for(int i = 0; i < s.size(); i++)
+           	//         {
+           	//             if(iswalnum(s[i]))	//iswalnum is to check whether s[i] is alphanumeric or not
+           	//             {
+           	//                 ans.push_back(s[i]);
+           	//             }
+           	//         }
+
+           	//         transform(ans.begin(), ans.end(), ans.begin(), ::tolower);	//to make all character lower case
+
+           	//         output = ans;	//making a duplicate string
+
+           	//         reverse(output.begin(),output.end());	// reversing the duplicated string
+
+           	//         if(output == ans)	//checking if both the strings are equa; or not
+           	//         {
+           	//             return true;
+           	//         }
+           	//         else
+           	//         {
+           	//             return false;
+           	//         }
+
+            //This is a two pointer approach to do this in O(n), and without using extra space;
+            int l = 0;
+            int r = s.size() - 1;
+            while(l <= r)
             {
-                ans.push_back(s[i]);
+                if (!iswalnum(s[l]))
+                {
+                    l++;
+                    continue;
+                }
+                if (!iswalnum(s[r]))
+                {
+                    r--;
+                    continue;
+                }
+                if (tolower(s[l]) != tolower(s[r]))
+                {
+                    return 0;
+                }
+                else
+                {
+                    l++;
+                    r--;
+                }
             }
+            return 1;
         }
-        transform(ans.begin(), ans.end(), ans.begin(), ::tolower);
-        output = ans;
-        reverse(output.begin(),output.end());
-        if(output == ans)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-        
-    }
 };
