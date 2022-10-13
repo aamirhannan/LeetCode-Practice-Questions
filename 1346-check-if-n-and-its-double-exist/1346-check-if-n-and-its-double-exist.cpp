82dad0a57976c28d86aeb8a4e48be4a509f3facf
aamirhannan <56514008+aamirhannan@users.noreply.github.com>
2022-10-13 17:26:11 +0530

    Time: 14 ms (50.31%), Space: 10.4 MB (27.52%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,9 +1,10 @@
 class Solution
 {
     public:
-        bool checkIfExist(vector<int> &arr) 
+        bool checkIfExist(vector<int> &arr)
         {
-            for(int i = 0; i < arr.size(); i++)
+            // this soln has T.C----> O(n^2) and S.c----> O(1)
+            /*for(int i = 0; i < arr.size(); i++)
             {
                 for(int j = 0; j < arr.size(); j++)
                 {
@@ -16,6 +17,20 @@ class Solution
                         return true;
                     }
                 }
+            }
+            return false;
+            */
+            
+            
+            //this soln has O(n) T.C and O(n) S.C
+            unordered_map<double, int> mp;
+            for (int i = 0; i < arr.size(); i++)
+            {
+                if (mp.count((double) arr[i] *2) or mp.count((double) arr[i] / 2))
+                {
+                    return true;
+                }
+                mp[arr[i]]++;
             }
             return false;
         }
