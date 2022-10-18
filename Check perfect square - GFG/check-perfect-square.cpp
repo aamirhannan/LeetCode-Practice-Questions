ba3e47a84eb65e3ee0c13c3fd7e064c1cdedcb1b
aamirhannan <56514008+aamirhannan@users.noreply.github.com>
2022-10-18 21:48:41 +0530

    Added solution - LeetHub

---
+++ Check perfect square - GFG/check-perfect-square.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int checkPerfectSquare(int N)
+    {
+        // code here 
+        for(int i = 1; i <= sqrt(N); i++)
+        {
+            if(i * i == N)
+            {
+                return 1;
+            }
+        }
+        
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.checkPerfectSquare(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
