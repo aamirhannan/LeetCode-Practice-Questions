6a1e63271b4981beb2a8e9e4fc81c369082220d4
aamirhannan <56514008+aamirhannan@users.noreply.github.com>
2022-10-18 20:59:26 +0530

    Added solution - LeetHub

---
+++ Swap two numbers - GFG/swap-two-numbers.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        //complete the function here
+    
+        pair<int, int> ans;
+        
+      
+        
+         a = abs(a + b);
+         b = abs(a - b);
+         a = abs(a - b);
+        
+        ans.first = a;
+        ans.second = b;
+        
+        return ans;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        
+        Solution ob;
+        pair<int, int>p = ob.get(a, b);
+        cout << p.first << ' ' << p.second << endl;
+    
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
