1d98b5ec2ea3731380083513f1144cedc68d260f
aamirhannan <56514008+aamirhannan@users.noreply.github.com>
2022-10-24 07:05:14 +0530

    Time: 23 ms (16.53%), Space: 11.2 MB (59.18%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -2,32 +2,31 @@ class Solution
 {
     public:
 
-        void solve(string ip, string op, vector<string> &v) 
+        void solve(string ip, string op, int idx, vector<string> &v) 
         {
             //Base case
-            if(ip.size() == 0)
+            if(idx == ip.size())
             {
                 v.push_back(op);
                 return;
             }
             //Hypothesis
-            if(isalpha(ip[0]))
+            if(isalpha(ip[idx]))
             {
                 string op1 = op;
                 string op2 = op;
-                op1.push_back(tolower(ip[0]));
-                op2.push_back(toupper(ip[0]));
-                ip.erase(ip.begin()+0);
+                op1.push_back(tolower(ip[idx]));
+                op2.push_back(toupper(ip[idx]));
                 
-                solve(ip,op1,v);
-                solve(ip,op2,v);
+                
+                solve(ip,op1,idx+1,v);
+                solve(ip,op2,idx+1,v);
             }
             else
             {
                 string op1 = op;
-                op1.push_back(ip[0]);
-                ip.erase(ip.begin()+0);
-                solve(ip,op1,v);
+                op1.push_back(ip[idx]);
+                solve(ip,op1,idx+1,v);
             }
             return;
         }
@@ -37,7 +36,7 @@ class Solution
         string ip = s;
         string ot = "";
         vector<string> v;
-        solve(ip, ot, v);
+        solve(ip, ot, 0, v);
 
         return v;
     }
