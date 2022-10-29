4247014f383e2214a6966c3f755f237f14555d28
aamirhannan <56514008+aamirhannan@users.noreply.github.com>
2022-10-29 18:26:09 +0530

    Time: 46 ms (39.11%), Space: 19.6 MB (60.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,7 +2,33 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) 
     {
+        //Approach - 1,Voting Algorithm; T.C - O(n), S.C - O(1);
+        
+        int member = 0;
+        int gang = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(member == 0)
+            {
+                member += 1;
+                gang = nums[i];
+            }
+            else if(nums[i] == gang)
+            {
+                member += 1;
+            }
+            else if(nums[i] != gang)
+            {
+                member -= 1;
+            }
+        }
+        
+        return gang;
+        
+        
+        /*Approach - 2; T.C - O(nlog(n)), S.C - O(1);
         sort(nums.begin(), nums.end());
         return nums[nums.size()/2];
+        */
     }
 };
